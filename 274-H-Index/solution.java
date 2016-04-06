96a2e7d96be6eb0921347bbfcb5552f4e3d5d0c5
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-05 18:08:48 -0600

    [274. H-Index][Accepted]committed by LeetCode Extension

---
+++ 274-H-Index/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+    	int len = citations.length;
+    	for(int i=0; i<len; i++){
+    		if(citations[i]>=len-i){
+    			return len-i;
+    		}
+    	}
+    	return 0;
+    }
+}
