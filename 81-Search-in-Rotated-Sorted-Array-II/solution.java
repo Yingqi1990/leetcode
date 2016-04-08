61c8dc72e8728b6e11cfa40b73c87c0b2d447976
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-08 12:05:11 -0600

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

---
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+     if(nums==null || nums.length==0) return false;
+     int left = 0;
+     int right = nums.length-1;
+     while(left<=right){
+         int mid = left+(right-left)/2;
+         if(nums[mid]==target) return true;
+         if(nums[mid]>nums[left]){
+            if(nums[left]<=target && target<nums[mid]){
+                     right = mid-1;
+            }else{
+                     left = mid+1; 
+            }
+         }else if(nums[mid] < nums[left]){
+            if(nums[mid]<target && target <=nums[right]){
+                    left = mid + 1;
+            }else{
+                    right = mid-1;    
+            }
+         }else{
+             left++;
+         }
+     }
+     return false;
+    }
+}
