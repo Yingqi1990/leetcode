f8c641bda038a2a13e53ae102f7e2fd3999c20ab
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-15 15:17:40 -0600

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -1,44 +1,43 @@
 public class Solution {
-     public int minCut(String s){
-	   if(s==null || s.length() == 0){
-		   return -1;
-	   }
-	   boolean[][] isPal = getPal(s);
-	   int[] dp = new int[s.length()+1];
-	   dp[0] = 0;
-	   for(int i =1; i <= s.length(); i++){
-		   dp[i] = Integer.MAX_VALUE;
-		   for(int j = 0; j <i ; j++ ){
-			   if(isPal[j][i-1]){
-			   dp[i] = Math.min(dp[i], dp[j]+1); 
-			   }
-		   }
-	   }
-	   return dp[s.length()]-1; 
-   }
-   
-   public boolean[][] getPal(String s){
-	   
-	   boolean[][] isPal = new boolean[s.length()][s.length()];
-	   
-	   for(int i = 0; i < s.length(); i++){
-		   isPal[i][i] = true;
-	   }
-	   
-	   for(int i=0; i<s.length() - 1; i++){
-		   if(s.charAt(i) == s.charAt(i+1)){
-		      isPal[i][i+1] = true;
-		   }
-	   }
-	   
-	   for(int len = 2; len < s.length(); len++){
-		 
-		   for(int i=0; i+len < s.length(); i++){
-		      isPal[i][i+len] = isPal[i+1][i+len-1] && (s.charAt(i) == s.charAt(i+len));	   
-		   }
-	   
-	   }
-	   
-	   return isPal;
-   }
+    public int minCut(String s) {
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        
+        boolean[][] palMatrix = getPal(s);
+        int[] dp = new int[s.length()+1];
+        dp[0] = 0;
+        for(int i = 1; i <= s.length(); i++){
+            dp[i] = Integer.MAX_VALUE;
+            for(int j = 1; j <= i; j++ ){
+                if(palMatrix[j-1][i-1] == true){
+                    dp[i] = Math.min(dp[i], dp[j-1]+1);
+                }
+            }
+        }
+        return dp[s.length()] - 1;
+    }
+    
+    public boolean[][] getPal (String s){
+        boolean[][] palMatrix = new boolean[s.length()][s.length()];
+        
+        for(int i = 0; i < s.length(); i++){
+            palMatrix[i][i] = true; 
+        }
+        
+        for(int i=1; i < s.length(); i++){
+           if(s.charAt(i-1) == s.charAt(i)){
+               palMatrix[i-1][i] = true;
+           }
+        }
+        
+        for(int len = 2; len < s.length(); len++){
+            for(int i = 0; i < s.length() - len; i++){
+                if(s.charAt(i) == s.charAt(i+len) && palMatrix[i+1][i+len-1] == true){
+                    palMatrix[i][i+len] = true;
+                }
+            }
+        }
+        return palMatrix;
+    }
 }
