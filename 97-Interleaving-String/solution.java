2d31de494f843ee8f4c08b0fc0f2a20a175c6005
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-15 18:39:33 -0600

    [97. Interleaving String][Accepted]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -1,30 +1,66 @@
 public class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
+     public boolean isInterleave(String s1, String s2, String s3) {
+        if(s3 == null){
             return false;
-        }
-        
-        boolean [][] interleaved = new boolean[s1.length() + 1][s2.length() + 1];
-        interleaved[0][0] = true;
-        
-        for (int i = 1; i <= s1.length(); i++) {
-            if(s3.charAt(i - 1) == s1.charAt(i - 1) && interleaved[i - 1][0])
-                interleaved[i][0] = true;
-        }
-        
-        for (int j = 1; j <= s2.length(); j++) {
-            if(s3.charAt(j - 1) == s2.charAt(j - 1) && interleaved[0][j - 1])
-                interleaved[0][j] = true;
-        }
-        
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                if(((s3.charAt(i + j - 1) == s1.charAt(i - 1) && interleaved[i - 1][j]))
-                    || ((s3.charAt(i + j - 1)) == s2.charAt(j - 1) && interleaved[i][j - 1]))
-                interleaved[i][j] = true;
+        }else if( s1 == null && s2 == null){
+            return false;
+        }else if( s1 == null && s2!= null){
+            if(s2.length() !=s3.length()){
+                return false;
+            }else{
+                for(int i=0; i < s3.length(); i++){
+                    if(s2.charAt(i) != s3.charAt(i)){
+                         return false;
+                    }
+                }
+                return true;
             }
-        }
-        
-        return interleaved[s1.length()][s2.length()];
-    }
+        }else if( s1 != null && s2 == null ){
+           if(s1.length() !=s3.length()){
+                return false;
+            }else{
+                for(int i=0; i < s3.length(); i++){
+                    if(s1.charAt(i) != s3.charAt(i)){
+                         return false;
+                    }
+                }
+                return true;
+            } 
+        }else{
+             if(s3.length() != s1.length() + s2.length()){
+                 return false;
+             }else{
+                 int len1 = s1.length();
+                 int len2 = s2.length();
+                 boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+                 dp[0][0] = true;
+                 
+                 for(int i = 1; i <= len1; i++){
+                     if(s1.charAt(i-1) == s3.charAt(i-1) && dp[i-1][0]){
+                          dp[i][0] = true;   
+                     }
+                 }
+                 
+                 for(int i = 1; i <= len2; i++){
+                     if(s2.charAt(i-1) == s3.charAt(i-1) && dp[0][i-1]){
+                         dp[0][i] =true;
+                     }
+                 }
+                 
+                 for(int i = 1; i <= len1; i++){
+                     for(int j = 1; j <= len2; j++){
+                         if(s1.charAt(i-1) == s3.charAt(i+j-1) && dp[i-1][j]){
+                             dp[i][j] = true;
+                         }else if((s2.charAt(j-1) == s3.charAt(i+j-1)) && dp[i][j-1]){
+                             dp[i][j] = true;
+                         }else{
+                             dp[i][j] = false;
+                         }
+                     }
+                 }
+                 
+                 return dp[len1][len2];
+             }
+        } 
+     }
 }
