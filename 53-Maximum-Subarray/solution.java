2ddfaafc771cb974e7d19c66af89aa8ad68967c3
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-17 15:17:02 -0600

    [53. Maximum Subarray][Accepted]committed by LeetCode Extension

---
+++ 53-Maximum-Subarray/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int local = nums[0];
+        int global = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            local = Math.max(local + nums[i], nums[i]);
+            global = Math.max(local, global);
+        }
+        
+        return global;
+    }
+}
