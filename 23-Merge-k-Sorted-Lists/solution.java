0faae918c4eb0d4eb41839881d2a8637c8711d97
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-18 18:36:00 -0600

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

---
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0){
+            return null;
+        }
+        
+        return helper(lists, 0, lists.length - 1);
+    }
+    
+    public ListNode helper(ListNode[] lists, int left, int right){
+            while(left < right){
+                int mid = left + (right - left)/2;
+                 return merge(helper(lists, left, mid), helper(lists, mid +1, right));
+            } 
+            return lists[left];
+    }
+    
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        dummy.next = l1;
+        ListNode cur = dummy;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                l1 = l1.next;
+            }else{
+                cur.next = l2;
+                ListNode next = l2.next;
+                l2.next = l1;
+                l2 = next;
+            }
+            cur = cur.next;
+        }
+        
+        if(l2!= null){
+            cur.next = l2;
+        }
+        
+        return dummy.next;
+    }
+    
+}
