d65a3b7cd293c2e6087a28b743fabffc4d6040f5
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-19 09:48:29 -0600

    [240. Search a 2D Matrix II][Accepted]committed by LeetCode Extension

--- 240-Search-a-2D-Matrix-II/solution.java
+++ 240-Search-a-2D-Matrix-II/solution.java
@@ -1,17 +1,20 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    if(matrix==null || matrix.length==0 || matrix[0].length==0) return false;
-    int x= matrix.length-1;
-    int y=0;
-    while((x>=0)&&(y<matrix[0].length)){
-        if(matrix[x][y]==target) return true;
-        if(matrix[x][y]<target){
-            y++;
-            
-        }else{
-            x--;
-        } 
-    }
-    return false;
+       if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+           return false;
+       }
+       int x = matrix.length - 1;
+       int y = 0;
+       
+       while((x >= 0) && (y < matrix[0].length)){
+           if(matrix[x][y] == target){
+               return true;
+           }else if(matrix[x][y] > target){
+               x--;
+           }else{
+               y++;
+           }
+       }
+       return false;
     }
 }
