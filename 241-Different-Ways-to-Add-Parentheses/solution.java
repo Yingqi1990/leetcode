5884d887e4616359d8b441c9df59433aa31c8aee
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-18 21:56:24 -0600

    [241. Different Ways to Add Parentheses][Accepted]committed by LeetCode Extension

---
+++ 241-Different-Ways-to-Add-Parentheses/solution.java
@@ -0,0 +1,51 @@
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+       List<Integer> res = new ArrayList<Integer>();
+       
+       if(input == null || input.length() == 0){
+           return res;
+       }
+        
+       if(input.length() == 1 && (input.charAt(0) == '*' || input.charAt(0) == '+' || input.charAt(0) == '-')){
+           return res;
+       }
+       
+       
+       for(int i = 0; i < input.length(); i++){
+           char ch = input.charAt(i);
+            
+           if(ch == '*' || ch == '+' || ch == '-'){
+               List<Integer> left = diffWaysToCompute(input.substring(0, i));
+               List<Integer> right = diffWaysToCompute(input.substring(i+1));
+               for(int t : left){
+                   for(int s : right){
+                       switch(ch){
+                           case '+':
+                               res.add(s + t);
+                               break;
+                               
+                           case '-':
+                               res.add(t - s);
+                               break;
+                               
+                           case '*':
+                              res.add(s * t);
+                              break;
+                              
+                       }
+                   }
+               }    
+               
+           }
+           
+       }
+       
+       if(res.size() == 0){
+           res.add(Integer.parseInt(input));
+           return res;
+       }
+       
+       return res;
+    }
+
+}
