24cc9cadc81469344d573154cf5f1a48fe69e354
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-25 11:03:08 -0600

    [326. Power of Three][Accepted]committed by LeetCode Extension

---
+++ 326-Power-of-Three/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    
+    private static final double eps = 10e-15;
+    
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
+        
+        if(n == 1){
+            return true;
+        }
+        
+        double res = Math.log(n) / Math.log(3);
+        
+        return Math.abs(Math.round(res) - res) < eps;
+        
+    }
+}
