2124f01fe9fa70f95ee946ac894cd1968e44718f
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-04-27 23:47:13 -0600

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -1,27 +1,35 @@
 public class Solution {
     public boolean search(int[] nums, int target) {
-     if(nums==null || nums.length==0) return false;
-     int left = 0;
-     int right = nums.length-1;
-     while(left<=right){
-         int mid = left+(right-left)/2;
-         if(nums[mid]==target) return true;
-         if(nums[mid]>nums[left]){
-            if(nums[left]<=target && target<nums[mid]){
-                     right = mid-1;
-            }else{
-                     left = mid+1; 
-            }
-         }else if(nums[mid] < nums[left]){
-            if(nums[mid]<target && target <=nums[right]){
-                    left = mid + 1;
-            }else{
-                    right = mid-1;    
-            }
-         }else{
-             left++;
-         }
-     }
-     return false;
+      if(nums == null || nums.length == 0){
+          return false;
+      }
+      
+      int left = 0;
+      int right = nums.length - 1;
+      
+      while(left <= right){
+          int mid = left + (right - left) / 2;
+          
+          if(nums[mid] == target){
+              return true;
+          }else if(nums[left] < nums[mid]){
+              if(nums[left] <= target && target < nums[mid]){
+                  right = mid - 1;
+              }else{
+                  left = mid + 1;
+              }
+          }else if(nums[left] > nums[mid]){
+              if(nums[mid] < target && target <= nums[right]){
+                  left = mid + 1;
+              }else{
+                  right = mid - 1;
+              }
+          }else{
+              left++;
+          }
+        
+      }
+      
+      return false;
     }
 }
