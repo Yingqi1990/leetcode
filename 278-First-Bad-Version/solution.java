12a0b4332031ee8f90c4f191d15837194abc8298
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-05-01 23:22:06 -0600

    [278. First Bad Version][Accepted]committed by LeetCode Extension

--- 278-First-Bad-Version/solution.java
+++ 278-First-Bad-Version/solution.java
@@ -3,17 +3,23 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        if(n<0) return -1;
+        if(n < 1){
+            return -1;
+        }
+        
         int left = 1;
         int right = n;
-        while(left<=right){
-            int mid = left+(right-left)/2;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            
             if(isBadVersion(mid)){
-                 right = mid-1;
+                right = mid - 1;
             }else{
-                left = mid+1;
+                left = mid + 1;
             }
         }
-        return left>n? -1:left;
+        
+        return left > n ? -1 : left;
     }
 }
