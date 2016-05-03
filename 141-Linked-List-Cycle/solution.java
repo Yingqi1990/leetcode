a795b73fc35857ef37ff2f89bf02795a2e75d947
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-05-02 23:39:05 -0600

    [141. Linked List Cycle][Accepted]committed by LeetCode Extension

--- 141-Linked-List-Cycle/solution.java
+++ 141-Linked-List-Cycle/solution.java
@@ -18,12 +18,13 @@ public class Solution {
         ListNode slow = head;
         ListNode fast = head;
         
-        while(fast.next != null && fast.next.next !=null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast){
-                return true;
-            }
+        while(fast.next!=null && fast.next.next != null){
+              slow = slow.next;
+              fast = fast.next.next;
+              
+              if(fast == slow){
+                  return true;
+              }
         }
         
         return false;
