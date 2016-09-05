6de49273febf209a2b3b2d8fe4b42abfd1436d78
Yingqi1990 <wilsonyingqilu@hotmail.com>
2016-09-05 14:53:48 -0600

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -8,45 +8,43 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        ListNode prev = head;
-        int digit = 0;
-        int carry = 0;
-        while(l1!= null && l2!=null){
-            digit = (l1.val + l2.val + carry) % 10;
-            carry = (l1.val + l2.val + carry) / 10;
-            ListNode newNode = new ListNode(digit);
-            
-            if(head == null){
-                head = newNode;
-            }else{
-                prev.next = newNode;
-            }
-            
-            prev = newNode;
-            
-            l1 = l1.next;
-            l2 = l2.next;
-            
-        }
-        
-        while(l1!=null){
-            digit = (l1.val + carry) % 10;
-            carry = (l1.val + carry) / 10;
-            ListNode newNode = new ListNode(digit);
-            
-            if(head == null){
-                head = newNode;
-            }else{
-                prev.next = newNode;
-            }
-            
-            prev = newNode;
-            
-            l1 = l1.next;
-        }
-        
-        while(l2!=null){
+         ListNode head = null;
+         ListNode prev = head;
+         int digit = 0;
+         int carry = 0;
+         
+         while(l1 != null && l2 != null){
+             digit = (l1.val + l2.val + carry) % 10;
+             carry = (l1.val + l2.val + carry) / 10;
+             ListNode newNode = new ListNode (digit);
+             
+             if(head == null){
+                 head = newNode;
+             }else{
+                 prev.next = newNode;
+             }
+             
+             prev = newNode;
+             l1 = l1.next;
+             l2 = l2.next;
+         }
+         
+         while(l1 != null){
+             digit = (l1.val + carry) % 10;
+             carry = (l1.val + carry) / 10;
+             ListNode newNode = new ListNode(digit);
+             
+             if(head == null){
+                 head = newNode;
+             }else{
+                 prev.next = newNode;
+             }
+             
+             prev = newNode;
+             l1 = l1.next;
+         }
+         
+         while(l2 != null){
              digit = (l2.val + carry) % 10;
              carry = (l2.val + carry) / 10;
              ListNode newNode = new ListNode(digit);
@@ -58,15 +56,15 @@ public class Solution {
              }
              
              prev = newNode;
-             
-             l2 = l2.next; 
-        }
-        
-        if(carry != 0){
-            ListNode newNode = new ListNode(carry);
-            prev.next = newNode;
-        }
-        
-        return head;
+             l2 = l2.next;
+         }
+         
+         if(carry != 0){
+             ListNode newNode = new ListNode(carry);
+             prev.next = newNode;
+         }
+         
+         return head;
+         
     }
 }
